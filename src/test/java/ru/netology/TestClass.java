4421c1c53d14cc2dcb5998ccff3cfa73e6dfb431
Alinailka <alina-il@mail.ru>
2022-05-11 21:06:20 +0700

    Initial commit

---
+++ src/test/java/ru/netology/TestClass.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestClass {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @BeforeEach
+    public void setUp2() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void test() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Робертс Джулия");
+        driver.findElement(By.cssSelector("[type =tel]")).sendKeys("+79888888888");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[type=button]")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                text.trim());
+    }
+}
